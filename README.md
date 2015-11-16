c6fc0ec53f377b2b15dffce70c3d094b26491fc9
Wayne <ywj2167917@gmail.com>
2015-11-16 17:02:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+﻿# DropAnimationView
+---
+
+Using Android Property Animation to implement the animation of leaves falling.
+
+# Demo
+---
+![preview_drop_animation](preview_drop_animation.gif)
+
+
+# Usage
+---
+First, add following code into your layout xml
+
+    <com.waynell.library.DropAnimationView
+        android:id="@+id/drop_animation_view"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        app:minSize="30dp"
+        app:maxSize="50dp"
+        app:largePercent="80%"
+        app:rate="100"
+        app:xAnimate="true"
+        app:yAnimate="true"
+        app:rotationAnimate="true"/>
+
+Second, find this view and set drawables id
+
+    DropAnimationView view = (DropAnimationView) findViewById(R.id.drop_animation_view);
+    view.setDrawables(R.drawable.leaf_1,
+            R.drawable.leaf_2,
+            R.drawable.leaf_3,
+            R.drawable.leaf_4,
+            R.drawable.leaf_5,
+            R.drawable.leaf_6);
+
+finally, call startAnimation() begin doing animation.
+
---
+++ app/src/main/java/com/waynell/leafanimation/MainActivity.java
@@ -0,0 +1,49 @@
+package com.waynell.leafanimation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.waynell.library.DropAnimationView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+        DropAnimationView view = (DropAnimationView) findViewById(R.id.drop_animation_view);
+        view.setDrawables(R.drawable.leaf_1,
+                R.drawable.leaf_2,
+                R.drawable.leaf_3,
+                R.drawable.leaf_4,
+                R.drawable.leaf_5,
+                R.drawable.leaf_6);
+        view.startAnimation();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+}
